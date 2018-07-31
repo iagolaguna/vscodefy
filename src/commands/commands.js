397e396907c99d1ce2e006faf23c8144206d9444
Iago Laguna <iagolaguna.83@gmail.com>
2018-07-31 00:54:36 -0300

    init project

---
+++ src/commands/commands.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import PubSub from 'pubsub-js';
+const token = 'BQCi7H5G_gWYBYP1-iJLtpaOWNeAWdXPqCTxtItro5YlEGytuP9Ywo3WOEpYP-R9rzdAmp4zd0Zi2Lpt1K9tp07SJvJT0yuHWHG4dRd1w0bSy_PJ_igcpKqEJF_QhymaE6p5W5h4TMU58OhOTH6oS-Z4inmDfPiNN-mM';
+const prefix = 'Bearer';
+const totalToken = `${prefix} ${token}`;
+const spotifyUrl = 'https://api.spotify.com/v1/me/player';
+
+async function next () {
+  try {
+    await axios
+      .post(`${spotifyUrl}/next`, {}, {
+        headers: {
+          'Authorization': totalToken
+        }
+      });
+    PubSub.publish('next', null);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function previous () {
+  try {
+    await axios
+      .post(`${spotifyUrl}/previous`, {}, {
+        headers: {
+          'Authorization': totalToken
+        }
+      })
+    PubSub.publish('previous', null);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function pause () {
+  try {
+    await axios
+      .put(`${spotifyUrl}/pause`, {}, {
+        headers: {
+          'Authorization': totalToken
+        }
+      })
+    PubSub.publish('pause', null)
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function play () {
+  try {
+    await axios
+      .put(`${spotifyUrl}/play`, {}, {
+        headers: {
+          'Authorization': totalToken
+        }
+      })
+    PubSub.publish('play', null)
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function signIn () {
+  PubSub.publish('signIn', null)
+}
+
+export {
+  previous,
+  next,
+  pause,
+  play,
+  signIn
+};
---
+++ src/extension.js
@@ -0,0 +1,97 @@
+import 'babel-polyfill';
+import PubSub from 'pubsub-js';
+import { window, commands, Disposable, StatusBarAlignment } from 'vscode';
+import { play, pause, next, previous, signIn } from './commands/commands';
+
+function activate ({ subscriptions }) {
+  const commandsRegistered = [
+    {
+      command: 'vscodefy.next',
+      action: next
+    },
+    {
+      command: 'vscodefy.previous',
+      action: previous
+    },
+    {
+      command: 'vscodefy.play',
+      action: play
+    },
+    {
+      command: 'vscodefy.pause',
+      action: pause
+    },
+    {
+      command: 'vscodefy.signIn',
+      action: signIn
+    }
+  ];
+
+  const reference = commandsRegistered
+    .map(({ command, action }) => commands.registerCommand(command, action));
+  subscriptions.push(Disposable.from(...reference));
+
+  const siginStatusBar = window.createStatusBarItem(StatusBarAlignment.Left, 11);
+  siginStatusBar.text = 'Sing In';
+  siginStatusBar.command = 'vscodefy.signIn';
+  siginStatusBar.tooltip = 'Entrar no Spotify';
+  siginStatusBar.show();
+
+  PubSub.subscribe('signIn', () => {
+    console.log('signIn called');
+    siginStatusBar.hide();
+    siginStatusBar.dispose();
+    const StatusBarButtons = buttonsInfo
+      .map(({ text, priority, buttonCommand, tooltip }) => {
+        const status = window.createStatusBarItem(StatusBarAlignment.Left, priority);
+        status.text = text;
+        status.command = buttonCommand;
+        status.tooltip = tooltip
+        status.show();
+        return status;
+      });
+    subscriptions.push(StatusBarButtons);
+  });
+  // subscriptions.push(StatusBarButtons);
+  // subscriptions.push(Disposable.from(...reference));
+}
+
+// this method is called when your extension is deactivated
+function deactivate () {
+}
+
+export {
+  activate,
+  deactivate
+}
+
+const buttonsInfo = [
+  {
+    id: 'next',
+    text: '$(chevron-right)',
+    priority: 8,
+    tooltip: 'Next',
+    buttonCommand: 'vscodefy.next'
+  },
+  {
+    id: 'play',
+    text: '$(triangle-right)',
+    priority: 9,
+    tooltip: 'Play',
+    buttonCommand: 'vscodefy.play'
+  },
+  {
+    id: 'pause',
+    text: '$(primitive-square)',
+    priority: 10,
+    tooltip: 'Pause',
+    buttonCommand: 'vscodefy.pause'
+  },
+  {
+    id: 'previous',
+    text: '$(chevron-left)',
+    priority: 11,
+    tooltip: 'Previoues',
+    buttonCommand: 'vscodefy.previous'
+  }
+];
