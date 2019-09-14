a88e15a732020fd91eb324cb5e142f1dfe2e057d
iagolaguna <iagolaguna.83@gmail.com>
2019-09-14 20:13:47 -0300

    migration to typescript first step

--- src/constant/index.js
+++
@@ -1,4 +0,0 @@
-export const SPOTIFY_PLAYER_URL = 'https://api.spotify.com/v1/me/player'
-export const OAUTH_SERVER_URL = 'https://vscodefy.herokuapp.com/api'
-export const OAUTH_SITE_URL = 'https://vscodefy.netlify.com'
-export const VSCODEFY_CACHE = 'vscodefy.cache'
